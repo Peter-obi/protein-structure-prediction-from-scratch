d61b5d71926b8602e86cdb79f96cfe3a835a6b54
Peter Obi <52758657+Peter-obi@users.noreply.github.com>
2026-03-01 22:22:37 -0500

    Update toy_diffusion.py

--- Part-2-Score-matching-basics/toy_diffusion.py
+++ Part-2-Score-matching-basics/toy_diffusion.py
@@ -12,11 +12,16 @@ categorical_dist = D.Categorical(probs=weights) # select the mode
 component_dist = D.Normal(means, stdev)
 gmm = D.MixtureSameFamily(categorical_dist, component_dist)
 samples = gmm.sample((1000,)) 
-samples.shape
 
-timesteps = 200
 betas = torch.linspace(0.0001, 0.02, 200)
 alphas = 1 - betas
 alphas_bar = torch.cumprod(alphas, dim=0)
 sqrt_aphas_cumprod = torch.sqrt(alphas_bar)
 sqrt_one_minus_alphas_cumprod = torch.sqrt(1 - alphas_bar)
+
+t = torch.zeros([128])
+embed_dim = 64
+half_dim = embed_dim//2
+frequencies = torch.exp(torch.arange(0, half_dim) * -torch.log(torch.tensor(10000.0))/(half_dim - 1))
+args = t.unsqueeze(-1) * frequencies.unsqueeze(0)
+embedding = torch.cat((torch.sin(args), torch.cos(args)), dim=-1)
