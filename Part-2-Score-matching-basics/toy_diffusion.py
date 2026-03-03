403f2b8b680e31242857ff75aff5f0ad751c816c
Peter Obi <52758657+Peter-obi@users.noreply.github.com>
2026-03-02 19:35:08 -0500

    Update toy_diffusion.py

--- Part-2-Score-matching-basics/toy_diffusion.py
+++ Part-2-Score-matching-basics/toy_diffusion.py
@@ -28,3 +28,36 @@ def get_timestep_embedding(t, embed_dim):
   args = t.unsqueeze(-1) * frequencies.unsqueeze(0)
   embedding = torch.cat((torch.sin(args), torch.cos(args)), dim=-1)
   return embedding
+
+class DiffusionModel(torch.nn.Module):
+  def __init__(self):
+    super(DiffusionModel, self).__init__()
+    self.time_proj = torch.nn.Sequential(
+        torch.nn.Linear(embed_dim, embed_dim),
+        torch.nn.SiLU(),
+        torch.nn.Linear(embed_dim, embed_dim)
+        )
+    self.data_proj = torch.nn.Sequential(
+        torch.nn.Linear(1, embed_dim),
+        torch.nn.SiLU(),
+        torch.nn.Linear(embed_dim, embed_dim)
+        )
+    self.hidden1 = torch.nn.Linear(embed_dim, 128)
+    self.activation = torch.nn.SiLU()
+    self.hidden2 = torch.nn.Linear(128, 128)
+    self.hidden3 = torch.nn.Linear(128, 128)
+    self.output = torch.nn.Linear(128, 1)
+
+  def forward(self, t, x):
+    t = get_timestep_embedding(t, embed_dim)
+    t = self.time_proj(t)
+    x = self.data_proj(x)
+    merge_proj = t + x
+    merge_proj = self.hidden1(merge_proj)
+    merge_proj = self.activation(merge_proj)
+    merge_proj = self.hidden2(merge_proj)
+    merge_proj = self.activation(merge_proj)
+    merge_proj = self.hidden3(merge_proj)
+    merge_proj = self.activation(merge_proj)
+    out = self.output(merge_proj)
+    return out
